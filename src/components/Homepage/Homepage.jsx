4afa8baa86b559fa4cf8bcf18de6346d4be1afca
merwhite11 <meredith.white91@gmail.com>
2024-04-26 13:49:14 -0700

    Nav Links styled

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Col, Row, Container, Header } from 'react-bootstrap';
-import './Homepage.scss';
+import React, { useState } from "react";
+import { Col, Row, Container, Header } from "react-bootstrap";
+import "./Homepage.scss";
 // import '../../scss/main.scss'
 // import NavBar from '../NavBar.jsx';
 // import Writing from './Writing.jsx';
@@ -8,7 +8,6 @@ import './Homepage.scss';
 // import About from './About.jsx';
 
 const Homepage = () => {
-
   // const [clickedLink, setClickedLink] = useState('');
 
   // const updateNavLink = (href) => {
@@ -17,35 +16,24 @@ const Homepage = () => {
 
   return (
     //hompage class here
-      <Container className="d-flex text-center">
-        <div className="homepage d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <Container className="d-flex text-center">
+      <div className="homepage d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header className="masthead mb-auto">
-            <div>
-            <h3 className="float-md-start mb-0 mt-3">Meredith White</h3>
-            <nav className="nav nav__masthead justify-content-center float-md-end">
-              <a className="nav__link fw-bold py-1 px-0 active">Writing</a>
+          <div className="mt-md-3">
+            <h3 className="float-md-start mb-0">Meredith White</h3>
+            <nav className="justify-content-center float-md-end">
+              <a className="nav__link fw-bold py-1 px-0">Writing</a>
               <a className="nav__link fw-bold py-1 px-0">Dance</a>
               <a className="nav__link fw-bold py-1 px-0">About</a>
             </nav>
-            </div>
+          </div>
         </header>
-        <Row className="homepage-row align-itemsjustify-content-center">
-          <Col className="col-6 align-self-center text-center">Pool of Work</Col>
-        </Row>
-        </div>
-      </Container>
-      /* <section id="welcome" className="welcome-section">
-        <nav>
-          <NavBar />
-        </nav>
-        <Container className="welcome-container d-flex align-items-center justify-content-center">
-          Pool of Work
-        </Container>
-      </section>
-      <section> <Writing /> </section>
-      <section> <Dance /></section>
-      <section> <About /></section> */
+        <main className="cover">
+          <h1 className="title">Pool of Work</h1>
+        </main>
+      </div>
+    </Container>
   );
-}
+};
 
 export default Homepage;
