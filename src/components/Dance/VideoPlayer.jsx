f23acfb134a57a7cf42512e8df5a3f2bbed37ade
merwhite11 <meredith.white91@gmail.com>
2024-06-27 14:16:29 -0700

    isPlaying for orientation playback continuity

--- src/components/Dance/VideoPlayer.jsx
+++ src/components/Dance/VideoPlayer.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Hls from 'hls.js';
 
 const VideoPlayer = ({ videoName }) => {
 
   const videoRef = useRef(null);
   const hlsRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false)
   const cdnUrl = process.env.CDN_URL
   const hlsSource = `${cdnUrl}/${videoName}/hls/${videoName}.m3u8`
   const mp4Source = `${cdnUrl}/${videoName}/mp4/${videoName}.mp4`
   const thumbnailSource = `${cdnUrl}/${videoName}/thumbnails/${videoName}.jpg`
 
-  const initializeVid = () => {
+  const initializeVid = (autoplay) => {
     const video = videoRef.current;
 
     if (Hls.isSupported()) {
@@ -21,19 +22,50 @@ const VideoPlayer = ({ videoName }) => {
       hlsRef.current = hls;
       hls.loadSource(hlsSource);
       hls.attachMedia(video);
+      if(autoplay) {
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          video.play();
+          setIsPlaying(true);
+        });
+      }
     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
       video.src = hlsSource;
+      if (autoplay) {
+        video.addEventListener('loadedmetadata', () => {
+          video.play();
+          setIsPlaying(true);
+        });
+      }
     } else {
       video.src = mp4Source;
+      if (autoplay) {
+        video.addEventListener('loadedmetadata', () => {
+          video.play();
+          setIsPlaying(true);
+        });
+      }
     }
   };
 
+  const debounce = (func, wait) => {
+    let timeout;
+    return function (...args) {
+      const context = this;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+  };
+
+  const handleOrientationChange = debounce(() => {
+    if (isPlaying) {
+      initializeVid(true)
+    }
+  }, 300);
+
   useEffect(() => {
-    initializeVid();
-    const handleOrientationChange = debounce(() => {
-      initializeHls();
-    }, 300);
+    initializeVid(false);
     window.addEventListener('orientationchange', handleOrientationChange);
+
     return () => {
       if (hlsRef.current) {
         hlsRef.current.destroy();
