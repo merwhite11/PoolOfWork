aad5cf34c27900b420b4b5917d127bf3e4c95e0f
merwhite11 <meredith.white91@gmail.com>
2025-01-31 16:08:53 -0800

    fixed token ref again

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = (env, argv) => {
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         'process.env.PUBLIC_URL': JSON.stringify('/'),
         'process.env.CDN_URL': JSON.stringify(process.env.CDN_URL),
-        'process.env.DATA_TOKEN': JSON.stringify(process.env.data-token)
+        'process.env.DATA_TOKEN': JSON.stringify(process.env.DATA_TOKEN)
       }),
       new MiniCssExtractPlugin({
         filename: "[name].css",
